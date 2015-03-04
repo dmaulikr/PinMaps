385c54d36cf4dade4ab899ac429dd068c3fd61fe
PresleyDias <presley.diaslee@gmail.com>
2015-03-04 23:22:51 +0530

    This commit does not work!
    This commit does not work!

---
+++ source/PinMaps/PinMaps/pinMapsMainView.h
@@ -0,0 +1,14 @@
+//
+//  pinMapsMainView.h
+//  PinMaps
+//
+//  Created by Presley on 04/03/15.
+//  Copyright (c) 2015 SlaaySourceCoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pinMapsMainView : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+@end
---
+++ source/PinMaps/PinMaps/pinMapsMainView.m
@@ -0,0 +1,63 @@
+//
+//  pinMapsMainView.m
+//  PinMaps
+//
+//  Created by Presley on 04/03/15.
+//  Copyright (c) 2015 SlaaySourceCoders. All rights reserved.
+//
+
+#import "pinMapsMainView.h"
+#import "SWRevealViewController.h"
+
+@interface pinMapsMainView ()
+
+@end
+
+@implementation pinMapsMainView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+        [self setSlideaction];
+}
+
+-(void)setSlideaction {
+    // Change button color
+    _sidebarButton.tintColor = [UIColor colorWithWhite:0.1f alpha:0.9f];
+    
+    // Set the side bar button action. When it's tapped, it'll show up the sidebar.
+    _sidebarButton.target = self.revealViewController;
+    _sidebarButton.action = @selector(revealToggle:);
+    
+    // Set the gesture
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
